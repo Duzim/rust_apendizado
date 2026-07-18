df8f096f0a478a6af2b4eca703c2dcd06c446a4b
Duzim <eduardocastrodonascimento@gmail.com>
2026-07-18 18:35:32 -0300

    more cap 12

--- cap12/minigrep/src/main.rs
+++ cap12/minigrep/src/main.rs
@@ -1,4 +1,6 @@
+use minigrep::{search, search_case_insensitive};
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -12,18 +14,26 @@ fn main() {
     });
 
     println!("Searching for {}...", config.query);
-    println!("In file {}", config.file_path);
+    println!("In file {} \n", config.file_path);
 
-    //Lendo o arquivo
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 
-    println!("With text: \n\n{contents}");
+    // match run(config) {
+    //     Ok(_) => (),
+    //     Err(e) => {
+    //         println!("Application error: {e}");
+    //         process::exit(1);
+    //     }
+    // }
 }
 
-struct Config {
-    query: String,
-    file_path: String,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 impl Config {
     fn build(args: &[String]) -> Result<Self, &'static str> {
@@ -33,13 +43,28 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Self { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Self { 
+            query, 
+            file_path,
+            ignore_case,
+        })
     }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
