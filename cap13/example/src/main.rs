23b77637a6162980fe5b4de323b6927cd082551d
Duzim <eduardocastrodonascimento@gmail.com>
2026-07-25 01:49:52 -0300

    add example

--- cap13/example/src/main.rs
+++ cap13/example/src/main.rs
@@ -1,12 +1,3 @@
-# [Recursos de Linguagem Funcional: Iteradores e Closures](https://doc.rust-lang.org/book/ch13-00-functional-features.html#functional-language-features-iterators-and-closures)
-
-## [Closures](https://doc.rust-lang.org/book/ch13-01-closures.html)
-
-As *`closures`* do Rust são funções anônimas que você pode armazenar em uma variável ou passar como argumentos para outras funções. Você pode criar a *`closure`* em um ponto e, posteriormente, invocá-la em outro local para executá-la em um contexto diferente. Ao contrário das funções, as *`closures`* podem capturar valores do escopo em que foram definidas. Demonstraremos como essas características das *`closures`* possibilitam a reutilização de código e a personalização de comportamento.
-
-### [Capturando o ambiente](https://doc.rust-lang.org/book/ch13-01-closures.html#capturing-the-environment)
-
-```rust
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum ShirtColor {
     Red,
@@ -60,4 +51,3 @@ fn main() {
         user_prefix2, giveaway2
     );
 }
-```
